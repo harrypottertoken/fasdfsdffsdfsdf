02fb21d2581146af43d4a8126415749ecee4fbeb
harrypottertoken <harrypotterfantoken@gmail.com>
2023-07-06 15:50:02 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Coin from "./Components/Coin/Coin";
+
+function App() {
+  return (
+    <div className="text">
+      <Coin />
+    </div>
+  );
+}
+
+export default App;
